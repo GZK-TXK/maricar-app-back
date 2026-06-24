5a3e056154bda9e86839e8d1a14f711f5dee51bd
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-06-24 16:57:25 +0200

    Creada rama user

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: trusted,
+    }
+})
